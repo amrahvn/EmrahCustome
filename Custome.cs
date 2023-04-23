297c6f2c8acaefaf9e7ceb332aecebf647ce338b
Amrahvn <amrahvn@code.edu.az>
2023-04-23 15:51:29 +0400

    add

---
+++ Custome.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace EmrahCustome
+{
+   public class CustomList<T>
+    {
+        private List<T> items;
+
+        public CustomList()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Find(Predicate<T> predicate)
+        {
+            return items.Find(predicate);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using EmrahCustome;
+
+class MyClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class Program
+{
+    public void Main(string[] args)
+    {
+        CustomList<MyClass> myList = new CustomList<MyClass>();
+
+        myList.Add(new MyClass { Id = 1, Name = "Emrah" });
+        myList.Add(new MyClass { Id = 2, Name = "Emir" });
+        myList.Add(new MyClass { Id = 3, Name = "RAh" });
+
+        MyClass result = myList.Find(x => x.Id == 1);
+
+        if (result != null)
+        {
+            Console.WriteLine("Name:" + result.Name);
+        }
+        else
+        {
+            Console.WriteLine("Object not found");
+        }
+    }
+
+
+}
